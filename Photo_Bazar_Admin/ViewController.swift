d46a4caacbf3e9b8ffcda952cd94621577fac88e
Mahmud Mostofa <mahmudmostofa@Mahmuds-Mac.local>
2020-01-23 12:26:13 +0600

    initial commit

---
+++ Photo_Bazar_Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo_Bazar_Admin
+//
+//  Created by Mahmud Mostofa on 23/1/20.
+//  Copyright © 2020 Mahmud Mostofa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
